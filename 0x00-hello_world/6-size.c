b2179ba98fcd6d6f53d59bb20bcd906f5ad8ed81
dagm24 <tibebudag07@gmail.com>
2023-02-16 12:14:39 -0800

    sizeof

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int l;
+	long int m;
+	long long int n;
+	char o;
+	float p;
+	 printf("The size of an int is: %lu.\n", (unsigned long)sizeof(l));
+	 printf("The size of a long long int is: %lu.\n", (unsigned long)sizeof(m));
+	 printf("The size of a long long int is: %lu.\n", (unsigned long)sizeof(n));
+	 printf("The size of a char is: %lu.\n", (unsigned long)sizeof(o));
+	 printf("The size of a float is: %lu.\n", (unsigned long)sizeof(p));
+	 
+	 return (0);
+}
