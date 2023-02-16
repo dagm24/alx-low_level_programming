146f3017538e9f69e7a61107dfe2be12daa594cc
dagm24 <tibebudag07@gmail.com>
2023-02-16 10:01:22 -0800

    Hello,printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /**
  * main - printf function
- * Return:0 (Success)
+ * Return: 0 (Success)
  */
-
 int main(void)
 {
 	printf("with proper grammer, but the outcome is a piece of art,\n")
