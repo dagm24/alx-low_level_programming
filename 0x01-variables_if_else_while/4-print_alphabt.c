73c485834eabfe929c958b2faf6ccecf804cb90c
dagm24 <tibebudag07@gmail.com>
2023-02-17 12:17:52 -0800

    variables

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+char ch = 'b';
+while (ch <= 'z')
+{
+if (ch != 'e' && ch != 'q')
+putchar(ch);
+}
+ch++;
+putchar('\n');
+return (0);
+}
