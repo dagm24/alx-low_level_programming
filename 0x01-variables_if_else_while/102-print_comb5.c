54cf10204714679b2dd4a7fe64ff31f118f3c063
dagm24 <tibebudag07@gmail.com>
2023-02-20 10:11:53 -0800

    varibles

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,22 +6,22 @@
 int main(void)
 {
 int i, j;
-for (i = 0; i < 100; i++)
+for (j = 0; j <= 99; j++)
 {
-for (j = 0; j < 100; j++)
+for (j = j; i <= 99; i++)
 {
-if (i < 7)
+if (i != j)
 {
-putchar((i / 10) + 48);
-putchar((i % 10) + 48);
-putchar(' ');
 putchar((j / 10) + 48);
 putchar((j % 10) + 48);
-if (i != 98 || j != 99)
+putchar(' ');
+putchar((i / 10) + 48);
+putchar((i % 10) + 48);
+}
+if (j * 100 + i != 9899)
 {
 putchar(',');
 putchar(' ');
-}
 }
 }
 }
