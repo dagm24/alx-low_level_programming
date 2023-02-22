7c9f6528a27e6710fcd3270755e67ee861422661
dagm24 <tibebudag07@gmail.com>
2023-02-22 07:41:25 -0800

    functions.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * print_to_98 - print natural numbers to 98
+ * @n:number to start from
+ * Return: Always 0
+ */
+void print_to_98(int n)
+{
+if (n <= 98)
+{
+for (n <= 98; n++)
+{
+if (n == 98)
+{
+printf("%i\n", n);
+break;
+}
+else
+{
+printf("%d, ", n);
+}
+}
+}
+else
+{
+for (n >= 98; n--)
+{
+if (n == 98)
+{
+printf("%d\n", n);
+break;
+}
+else
+{
+printf("%d, ", n);
+}
+}
+}
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,7 +15,7 @@ c = a * b;
 if (c > 9)
 {
 d = c % 10;
-e = (c -d) / 10;
+e = (c - d) / 10;
 _putchar(44);
 _putchar(32);
 _putchar(e + '0');
