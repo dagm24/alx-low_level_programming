92545f9e8c6a4f762afdb4f86595a8c319468aaa
dagm24 <tibebudag07@gmail.com>
2023-02-22 06:33:00 -0800

    functions

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 -10 alphabets
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+char c = 'a';
+int n = 0;
+for (n = 0; n < 10; n++)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
+}
