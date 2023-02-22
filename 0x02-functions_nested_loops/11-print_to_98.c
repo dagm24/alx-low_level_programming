9e26e09344a5605df60ddf6a99c7987221f2c93e
dagm24 <tibebudag07@gmail.com>
2023-02-22 09:13:30 -0800

    functions

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,10 +11,10 @@ void print_to_98(int n)
 int i, j;
 if (n <= 98)
 {
-for (i=n; i <= 98; i++)
+for (i = n; i <= 98; i++)
 {
 if (i != 98)
-printf("%d, ",i);
+printf("%d, ", i);
 else if (i == 98)
 printf("%d\n", i);
 }
