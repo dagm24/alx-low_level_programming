6d3ccc8574b9ef076f3eda39517bea65f9202606
dagm24 <tibebudag07@gmail.com>
2023-02-22 06:52:24 -0800

    functions

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - check absolte value of integer
+ * @r:number being checked
+ * Return: Always 0
+ */
+int _abs(int r)
+{
+if (r > 0)
+{
+return (r);
+}
+else if (r <0)
+{
+return (r * -1);
+}
+else
+{
+return (r);
+}
+return (0);
+}
