a58e291568211caa7807b8ff7edcf649e935b86b
dagm24 <tibebudag07@gmail.com>
2023-02-22 07:59:45 -0800

    functions

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,7 +10,7 @@ void print_to_98(int n)
 {
 if (n <= 98)
 {
-for (;n <= 98; n++)
+for (; n <= 98; n++)
 {
 if (n == 98)
 {
@@ -25,7 +25,7 @@ printf("%d", n);
 }
 else
 {
-for (;n >= 98; n--)
+for (; n >= 98; n--)
 {
 if (n == 98)
 {
