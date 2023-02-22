be947fa43b4c1159a090fc55a252dd1678e399cb
dagm24 <tibebudag07@gmail.com>
2023-02-22 09:39:39 -0800

    functions

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,7 +9,7 @@ void print_times_table(int n)
 int i, j, k;
 if (n >= 0 && n <= 15)
 {
-for (i =  0; i <= n; j++)
+for (i =  0; i <= n; i++)
 {
 for (j = 0; j <= n; j++)
 {
@@ -18,7 +18,7 @@ if (j == 0)
 {
 _putchar(k + '0');
 }
-else if (k < 10 && j != 0 )
+else if (k < 10 && j != 0)
 {
 _putchar(',');
 _putchar(' ');
