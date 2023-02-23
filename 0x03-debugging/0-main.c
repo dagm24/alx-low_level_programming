d9ce35037667cef728c0e6b0c654dc41b4a771b5
dagm24 <tibebudag07@gmail.com>
2023-02-23 12:08:54 -0800

    debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main - Entry point
+ * main -Entry point
  * Return: Always 0
  */
 int main(void)
--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - causes an infinite loop
+ * main -causes an infinite loop
  * Return: 0
  */
 int main(void)
@@ -8,11 +8,10 @@ int main(void)
 int i;
 printf("Infinite loop incoming :(\n");
 i = 0;
-/*while (i < 10)
-*{
-*putchar(i);
-*}
-*/
+/*while (i < 10)*/
+/*{*/
+/*putchar(i);*/
+/*}*/
 printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
