583bb58b7306ac354b6b6965edd45d1ef05b5da4
dagm24 <tibebudag07@gmail.com>
2023-02-23 22:05:01 -0800

    functions

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -8,9 +8,9 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_triangle(int size);
 void print_number(int n);
-int _putchar(char);
-int _isupper(int);
-int _isdigit(int);
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
 int mul(int, int);
 
 #endif
