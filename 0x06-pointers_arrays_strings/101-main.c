f315e06bdd266d051fa93942c00ae2d76f5d67e6
dagm24 <tibebudag07@gmail.com>
2023-03-02 10:04:52 -0800

    pointers

---
+++ 0x06-pointers_arrays_strings/101-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main -check code
+ * Return: 0
+ */
+int main(void)
+{
+print_number(98);
+_putchar('\n');
+print_number(402);
+_putchar('\n');
+print_number(1024);
+_putchar('\n');
+print_number(0);
+_putchar('\n');
+print_number(-98);
+_putchar('\n');
+return (0);
+}
