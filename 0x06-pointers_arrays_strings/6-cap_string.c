66a2a096d1db6e54cf90ab7e008d25940817a2c3
dagm24 <tibebudag07@gmail.com>
2023-03-02 08:44:30 -0800

    pointers

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -4,14 +4,14 @@
  * @str: the string to be capitalized
  * Return: A pointer to the changed string
  */
-char *cap_string(char *)
+char *cap_string(char *str)
 {
 int index = 0;
 while (str[index])
 {
 while (!(str[index] >= 'a' && str[index] <= 'z'))
 index++;
-if (str[index - 1] == '' ||
+if (str[index - 1] == ' ' ||
 str[index - 1] == '\t' ||
 str[index - 1] == '\n' ||
 str[index - 1] == ',' ||
@@ -19,7 +19,7 @@ str[index - 1] == ';' ||
 str[index - 1] == '.' ||
 str[index - 1] == '!' ||
 str[index - 1] == '?' ||
-str[index - 1] == ""  ||
+str[index - 1] == '"' ||
 str[index - 1] == '(' ||
 str[index - 1] == ')' ||
 str[index - 1] == '{' ||
