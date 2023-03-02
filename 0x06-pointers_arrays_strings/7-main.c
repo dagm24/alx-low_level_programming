9ee73e69b8e11c3753afcd23e0c64a8fc2402d1e
dagm24 <tibebudag07@gmail.com>
2023-03-02 08:30:51 -0800

    pointers

---
+++ 0x06-pointers_arrays_strings/7-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main -checks the code
+ * Return: 0
+ */
+int main(void)
+{
+char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
+char *p
+p = leet(s);
+printf("%s", p);
+printf("%s", s);
+return (0);
+}
