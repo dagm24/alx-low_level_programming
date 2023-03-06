01a8849c276c3f61a90c7733a619fbbca8475b1d
dagm24 <tibebudag07@gmail.com>
2023-03-06 07:02:51 -0800

    pointers

---
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * simple_print_buffer -prints buffer in hexa
+ * @buffer:the address of memory to print
+ * @size:the size of the memeory to print
+ * Return:nothing
+ */
+void simple_print_buffer(char *buffer, unsigned int size)
+{
+unsigned int i;
+i = 0;
+while (i < size)
+{
+if (i % 10)
+{
+printf(" ");
+}
+if (! (i % 10) && i)
+{
+printf("\n");
+}
+printf("0x%02x", buffer[i]);
+i++;
+}
+printf("\n");
+}
