16e9f9193768ec24b98759034a4d7889ea6015ab
dagm24 <tibebudag07@gmail.com>
2023-03-06 07:20:14 -0800

    pointers

--- 0x07-pointers_arrays_strings/0-main.c
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -16,7 +16,7 @@ if (i % 10)
 {
 printf(" ");
 }
-if (! (i % 10) && i)
+if (!(i % 10) && i)
 {
 printf("\n");
 }
@@ -24,4 +24,17 @@ printf("0x%02x", buffer[i]);
 i++;
 }
 printf("\n");
+}
+/**
+ * main -checks the code
+ * Return: 0
+ */
+int main(void)
+{
+char buffer[98] = {0x00};
+simple_print_buffer(buffer, 98);
+_memset(buffer, 0x01, 95);
+printf("--------------------------------------------------\n");
+simple_print_buffer(buffer, 98);
+return (0);
 }
