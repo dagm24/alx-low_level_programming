ccaf238a2746b042537018bc7f6cfbf203d01af6
dagm24 <tibebudag07@gmail.com>
2023-03-06 23:37:00 -0800

    pointers

---
+++ 0x07-pointers_arrays_strings/7-main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - checks the code
+ * Return: 0
+ */
+int main(void)
+{
+char board[8][8] == {
+{'r', 'k', 'b', 'q', 'k', 'b', 'k', 'r'},
+{'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+{'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+{'R', 'K', 'B', 'Q', 'K', 'B', 'K', 'R'},
+};
+print_chessboard(board);
+return (0);
+}
