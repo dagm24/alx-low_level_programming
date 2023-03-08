d96731985f98846894e02def152bd238464f9f66
dagm24 <tibebudag07@gmail.com>
2023-03-08 05:05:14 -0800

    recursion

---
+++ 0x08-recursion/1-main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * main-checks the code
+ * Return: 0
+ */
+int main(void)
+{
+_print_rev_recursion("\nColton Walker");
+return (0);
+}
---
+++ 0x08-recursion/2-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main-checks the code
+ * Return:0
+ */
+int main(void)
+{
+int n;
+n = _strlen_recursion("Corbin Coleman");
+printf("%d\n", n);
+return (0);
+}
