2c3f645be09d53047dfe5c7da75922837d8a0b64
dagm24 <tibebudag07@gmail.com>
2023-03-09 03:07:02 -0800

    recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,4 +10,6 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
+int actual_sqrt_recursion(int n, int i);
+int actual_prime(int n, int i);
 #endif
