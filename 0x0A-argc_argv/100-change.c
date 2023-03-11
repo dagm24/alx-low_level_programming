b0e9062fdfd62fae52c54ac01ed88cad2b859170
dagm24 <tibebudag07@gmail.com>
2023-03-11 12:42:49 -0800

    argc,argv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -15,7 +15,15 @@ int coins[] = {25, 10, 5, 2, 1};
 if (argc != 2)
 {
 printf("Error\n");
+return (1);
+}
+i = atoi(argv[1]);
+result = 0;
+if (i < 0)
+{
+printf("0\n");
 return (0);
+}
 for (j = 0; j < 5 && i >= 0; j++)
 {
 while (i >= coins[j])
@@ -26,5 +34,4 @@ i -= coins[j];
 }
 printf("%d\n", result);
 return (0);
-}
 }
