573d704f8aa15a326e057baa6b307f6de2d5e4b1
dagm24 <tibebudag07@gmail.com>
2023-03-11 12:23:55 -0800

    argc,argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,20 +1,59 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 /**
- * main-prints the product of two arguments passed to it
- * @argc:counts of argumnets on command
- * @argv:pointer of the array of command line arguments
+ * _atoi-convertsva string to an integer
+ * @s:string to be converted
+ * Return:the int converted from the string
+ */
+int _atoi(char *s)
+{
+int i, d, n, len, f, digit;
+i = 0;
+d = 0;
+n = 0;
+len = 0;
+f = 0;
+digit = 0;
+while (s[len] != '\0')
+len++;
+while (i < len && f == 0)
+{
+if (s[i] == '-')
+++d;
+if (s[i] >= '0' && s[i] <= '9')
+{
+digit = s[i] - '0';
+if (d % 2)
+digit = -digit;
+n = n * 10 + digit;
+f = 1;
+if (s[i + 1] < '0' || s[i + 1] > '9')
+break;
+f = 0;
+}
+i++;
+}
+if (f == 0)
+return (0);
+return (n);
+}
+/**
+ * main-multiples 2 numbers
+ * @argc:number of arrguments
+ * @argv:array of arguments
  * Return: 0
  */
 int main(int argc, char *argv[])
 {
-int product;
-if (argc != 3)
+int mul, i, j;
+if (argc < 3 || argc > 3)
 {
 printf("Error\n");
 return (1);
 }
-product = _atoi(argv[1]) * _atoi(argv[2]);
-printf("%d\n", product);
+i = _atoi(argv[1]);
+j = _atoi(argv[2]);
+mul = i * j;
+printf("%d\n", mul);
 return (0);
 }
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * check_num-check string there are digit
+ * @str:array str
+ * Return: 0
+ */
+int check_num(char *str)
+{
+unsigned int count;
+count = 0;
+while (count < strlen(str))
+{
+if (!isdigit(str[count]))
+{
+return (0);
+}
+count++;
+}
+return (1);
+}
+/**
+ * main-prints the name of the program
+ * @argc:count arguments
+ * @argv:arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+int count;
+int str_to_int;
+int sum = 0;
+count = 1;
+while (count < argc)
+{
+if (check_num(argv[count]))
+{
+str_to_int = atoi(argv[count]);
+sum += str_to_int;
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+count++;
+}
+printf("%d\n", sum);
+return (0);
+}
