4047ddeaf3e9b2bdc0beeefc5397b2d6109e419b
dagm24 <tibebudag07@gmail.com>
2023-03-11 10:56:12 -0800

    argc.argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main- prints the name of the program
+ * @argc:number of arguments
+ * @argv:array of statements
+ * Return:0
+ */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+printf("%s\n", *argv - 1);
+return (0);
+}
---
+++ 0x0A-argc_argv/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * _putchar-writes the character to stdout
+ * @c:character to print
+ * Return: on success 1
+ * on error, -1 is returned, and errno is set appropriately
+ */
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
+
