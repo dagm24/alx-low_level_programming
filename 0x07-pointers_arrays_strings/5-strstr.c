c6bd2d59012503c4381f1325da46044af95474da
dagm24 <tibebudag07@gmail.com>
2024-01-08 07:31:38 -0800

    pointers,arrays and strings

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strstr-entry point
+ * @haystack:input
+ * @needle:input
+ * Return: 0
+ */
+char *_strstr(char *haystack, char *needle)
+{
+for (; *haystack != '\0'; haystack++)
+{
+char *i = haystack;
+char *p = needle;
+while (*i == *p && *p != '\0')
+{
+i++;
+p++;
+}
+if (*p == '\0')
+return (haystack);
+}
+return (0);
+}
