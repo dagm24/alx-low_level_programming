80239b44b5a476ad30159587e80127c7af8c7854
dagm24 <tibebudag07@gmail.com>
2024-01-08 13:30:34 -0800

    pointers,arrays and strings

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,11 +8,11 @@
  * Return:changed array with new value for n bytes
  */
 char *_memset(char *s, char b, unsigned int n)
-{
-unsigned int i;
-for (i = 0; i < n; i++)
-{
-s[i] = b;
-}
-return (s);
+	{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		*(s + i) =  b;
+
+	return (s);
 }
