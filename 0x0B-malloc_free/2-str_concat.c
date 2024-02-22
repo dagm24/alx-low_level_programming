8709534c9e7ae83a8bb8c7dec0cc54c895b12c1c
dagm24 <tibebudag07@gmail.com>
2024-02-22 12:29:26 -0800

    malloc free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,38 +8,25 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-char *s3;
-unsigned int i = 0;
-j = 0;
-len1 = 0;
-len2 = 0;
-while (s1 && s1[len1])
-len1++;
-while (s2 && s2[len2])
-len2++;
-s3 = malloc(sizeof(char) * (len1 + len2 + 1));
-if (s3 == NULL)
-return(NULL);
-i = 0;
-j = 0;
-if (s1)
-{
-while (i < len1)
-{
-s3[i] = s1[i];
-i++;
-}
-}
-if (s2)
-{
-while (i < (len1 + len2))
-{
-s3[i] = s2[i];
-i++;
-j++;
-}
-}
-s3[i] '\0';
-return (s3);
-}
+	int len1 = 0;
+	int len2 = 0;
+	int i;
+	char *concatstr;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (i = 0; s1[i] != '\0'; i++)
+		len1++;
+	for (i = 0; s2[i] != '\0'; i++)
+		len2++;
+	concatstr = malloc(sizeof(char) * (len1 + len2) + 1);
+	if (concatstr == NULL)
+		return (NULL);
+	for (i = 0; s1[i] != '\0'; i++)
+		concatstr[i] = s1[i];
+	for (i = 0; s2[i] != '\0'; i++)
+		concatstr[len1 + i] = s2[i];
+	return (concatstr);
+}
