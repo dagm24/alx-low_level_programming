46a3eeb6c8f4b898ff1526fed81e7e500b0fe185
dagm24 <tibebudag07@gmail.com>
2024-03-25 11:54:22 -0700

    dynamic libraries

---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _islower - Check Main
+ * @c: An input character
+ * Description: function uses _putchar function to print
+ * alphabet in lowercase 10 times
+ * Return: 1 if is lowercase or 0 if is uppercase
+ */
+int _islower(int c)
+{
+	char i;
+	int lower = 0;// Variable to indicate whether the character is lowercase
+        // Loop through the lowercase alphabet
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		// Check if the current character matches the input character
+		if (i == c)
+			// Set the 'lower' variable to 1 if it is a lowercase letter
+			lower = 1;
+	}
+        // Return the value of 'lower' (1 if lowercase, 0 if uppercase)
+	return (lower);
+}
---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_abs - Check Main
+ * @r: An integre input
+ * Description: This function returns absolute value of a number
+ * Return: Absolut value of number r
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+	// If true, return r as it is (already positive or zero)	
+	return (r);
+	else
+		// If false, return the negation of r (convert to positive)
+		return (r * -1);
+}
